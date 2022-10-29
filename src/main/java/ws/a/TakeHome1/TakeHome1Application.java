c84d567e5616e86b90a2c95d8ba9c8f5d1ddb1f2
prgtyfzh <pragitya.faazha.ft20@mail.umy.ac.id>
2022-10-29 19:55:44 +0700

    First Commit

---
+++ src/main/java/ws/a/TakeHome1/TakeHome1Application.java
@@ -0,0 +1,13 @@
+package ws.a.TakeHome1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeHome1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeHome1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ws/a/TakeHome1/TakeHome1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.a.TakeHome1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeHome1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
