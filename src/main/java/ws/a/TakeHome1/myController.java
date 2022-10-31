a01210a7de7d13d1f3b504e101349c03a4529506
prgtyfzh <pragitya.faazha.ft20@mail.umy.ac.id>
2022-10-31 15:34:42 +0700

    mengupdate index.html viewpage.html dan controller

--- src/main/java/ws/a/TakeHome1/myController.java
+++ src/main/java/ws/a/TakeHome1/myController.java
@@ -5,11 +5,15 @@
  */
 package ws.a.TakeHome1;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -19,17 +23,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class myController {
     
+    public static String UPLOAD_DIRECTORY = System.getProperty("user.dir") + "/src/main/resources/static";
+    
     @RequestMapping("/mainpage")
-
     public String hal(
     @RequestParam(value = "nama") String isinama,
             @RequestParam(value = "lokasi") String isilokasi,
-            @RequestParam(value = "gambar") String isigambar,
-            Model kiriman
-    ){
+         @RequestParam(value = "gambar") MultipartFile file,
+            Model kiriman)
+            throws IOException {
+        StringBuilder fileNames = new StringBuilder();
+        Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, file.getOriginalFilename());
+        fileNames.append(file.getOriginalFilename());
+        Files.write(fileNameAndPath, file.getBytes());
+        kiriman.addAttribute("msg", file.getOriginalFilename());
         kiriman.addAttribute("paketnama", isinama);
         kiriman.addAttribute("paketlokasi", isilokasi);
-        kiriman.addAttribute("paketgambar", isigambar);
         
         return "viewpage";
     }
